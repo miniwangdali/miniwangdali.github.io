672e55f2e3464698df9be1a9b1828942d47ceb7c
miniwangdali <wang__dali@sina.com>
2016-03-12 05:15:19 +0800

    构建左侧边栏
    1. 加入头像
    2. 目录居中
    3. 删除没用的东西
    4. 背景只是占位置

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 var rightframe;
+var leftheader;
+var portrait;
+
 
 function init(){
+    leftheader = document.getElementById("lHeader");
+    portrait = document.getElementById("portrait");
     rightframe = document.getElementById("rightframe");
     
 }
