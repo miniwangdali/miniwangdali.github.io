a058bf6dc5f9cfa3e8537624c74aec66681e223f
miniwangdali <wang__dali@sina.com>
2016-03-15 05:06:52 +0800

    尝试失败了！

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 var rightframe;
 var leftheader;
 var portrait;
-
+var rightcontent;
 
 function init(){
     leftheader = document.getElementById("lHeader");
     portrait = document.getElementById("portrait");
     rightframe = document.getElementById("rightframe");
-    
+    rightcontent = document.getElementById("content");
+    getResume("EN");
 }
