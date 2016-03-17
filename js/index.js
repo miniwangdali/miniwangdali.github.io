48c1505074c0c812da1874c93a7ddf448b55d437
miniwangdali <wang__dali@sina.com>
2016-03-18 06:00:51 +0800

    增加淡出

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ var leftheader;
 var portrait;
 var pagesframe;
 var startTime;
+var listOpen = false;
+var currentPos = 0;
+var iframe;
 
 function init(){
     leftheader = document.getElementById("lHeader");
@@ -11,18 +14,41 @@ function init(){
     pagesframe = document.getElementById("pages");
 }
 function gotoResume(){
-    pagesframe.src = "./resumeEN.html";
+    if(listOpen) return;
+    listOpen = true;
+    // pagesframe.src = "./resumeEN.html";
     var ul = document.getElementById("mainul");
+    var li = ul.getElementsByClassName("li");
     function onulEnd(){
         ul.style.animation = "";
         ul.style.webkitAnimation = "";
         ul.style.width = "50%";
+        for (var i = 0; i < li.length; i++) {
+            if (i != 1) {
+                li[i].style.animation = "";
+                li[i].style.webkitAnimation = "";
+                li[i].style.opacity = 0.5;
+                li[i].onmouseover = function(){
+                    this.style.opacity = 1;    
+                };
+                li[i].onmouseout = function(){
+                    this.style.opacity = 0.5;
+                };
+            }
+        }
+        li[1].style.backgroundColor = "#1976D2";
         openResume();
     }
     ul.addEventListener("webkitAnimationEnd", onulEnd);  // Chrome, Safari 和 Opera
     ul.addEventListener("animationend", onulEnd);        // 标准语法
-    ul.style.animation = "narrowWidth 2s";
-    ul.style.webkitAnimation = "narrowWidth 2s";
+    ul.style.animation = "narrowWidth 1.2s";
+    ul.style.webkitAnimation = "narrowWidth 1.2s";
+    for(var i = 0; i < li.length; i ++){
+        if(i != 1){
+            li[i].style.animation = "fadeOut 1.2s";
+            li[i].style.webkitAnimation = "fadeOut 1.2s";
+        }
+    }
 }
 function openResume(){
     var resumeList = document.getElementById("resumeList");
@@ -33,16 +59,21 @@ function openResume(){
     }
     resumeList.addEventListener("webkitAnimationEnd", onulEnd);  // Chrome, Safari 和 Opera
     resumeList.addEventListener("animationend", onulEnd);
-    resumeList.style.animation = "expandWidth 2s";
-    resumeList.style.webkitAnimation = "expandWidth 2s";
+    resumeList.style.animation = "expandWidth 1.2s";
+    resumeList.style.webkitAnimation = "expandWidth 1.2s";
     resumeList.style.display = "inline";
-    
+    pagesframe.src = "./resumeEN.html";
 }
 function resumeListFadeIn(parent){
     var children = parent.getElementsByClassName("subli");
     for(var i = 0; i < children.length; i ++){
         children[i].style.visibility = "visible";
-        children[i].style.animation = "fadeInVertical 2s";
-        children[i].style.webkitAnimation = "fadeInVertical 2s";
+        children[i].style.animation = "fadeInVertical 1.2s";
+        children[i].style.webkitAnimation = "fadeInVertical 1.2s";
     }
+    
+}
+function gotoPro(){
+    if(iframe == null) iframe = document.getElementById("pages");
+    scrollToOnIframe(iframe, "projects");
 }
---
+++ js/scroll.js
@@ -0,0 +1,36 @@
+function elementPosition(obj) {
+    var curleft = 0, curtop = 0;
+    if (obj.offsetParent) {
+        curleft = obj.offsetLeft;
+        curtop = obj.offsetTop;
+        while (obj = obj.offsetParent) {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+        }
+    }
+    return { x: curleft, y: curtop };
+}
+var repeatCount = 0;
+var clearTimeOut;
+
+function scrollSmoothly(iframe, pos){
+    if(iframe == null) iframe = window;
+    var times = pos / 50;
+    if(repeatCount < times){
+        iframe.contentWindow.scrollBy(0, 50);
+        repeatCount ++;
+        clearTimeOut = iframe.contentWindow.setTimeout("ScrollSmoothly('" + iframe + "','" + pos +"')", 100);
+    }else{
+        repeatCount = 0;
+        clearTimeout(clearTimeOut);
+    }
+}
+function scrollToOnIframe(iframe, id){
+    if(iframe == null) iframe = window;
+    var element = iframe.contentDocument.getElementById(id);
+    var scrollToPos = elementPosition(element).y;
+    scrollToPos -= iframe.contentDocument.documentElement.scrollTop;
+    var remainder = scrollToPos % 50;
+    scrollSmoothly(iframe, scrollToPos - remainder);
+    iframe.contentWindow.scrollBy(0, remainder);
+}
