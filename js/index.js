ef916e6777b0fcfac705bc820fd52f5343ac28f0
miniwangdali <wang__dali@sina.com>
2016-04-17 03:34:24 +0800

    尝试元素垂直居中

--- js/index.js
+++ js/index.js
@@ -25,14 +25,53 @@ function resizeNavi(mode) {
     switch (mode) {
         case 1:
             naviWrapper.style.width = "100%";
+            var header = document.getElementById("navi-header");
+            header.style.display = "none";
+            var menu = document.getElementById("menu");
+            var ul = menu.getElementsByTagName("ul")[0];
+            ul.style.padding = "0px 20px 0px 20px";
+            var lis = ul.getElementsByTagName("li");
+            for(var i = 0; i < lis.length; i ++){
+                lis[i].style.padding = "10px 20px 10px 20px";
+            }
+            var texts = menu.getElementsByClassName("text");
+            for(var i = 0; i < texts.length; i ++){
+                texts[i].style.display="table-cell";
+            }
             contentWrapper.style.width = "100%";
             break;
         case 2:
             naviWrapper.style.width = "60px";
+            var header = document.getElementById("navi-header");
+            header.style.display = "none";
+            var menu = document.getElementById("menu");
+            var ul = menu.getElementsByTagName("ul")[0];
+            ul.style.padding = "0";
+            var lis = ul.getElementsByTagName("li");
+            for(var i = 0; i < lis.length; i ++){
+                lis[i].style.padding = "10px 10px 10px 10px";
+            }
+            var texts = menu.getElementsByClassName("text");
+            for(var i = 0; i < texts.length; i ++){
+                texts[i].style.display="none";
+            }
             contentWrapper.style.width = width - 60 + "px";
             break;
         case 3:
             naviWrapper.style.width = "300px";
+            var header = document.getElementById("navi-header");
+            header.style.display = "inline";
+            var menu = document.getElementById("menu");
+            var ul = menu.getElementsByTagName("ul")[0];
+            ul.style.padding = "0px 20px 0px 20px";
+            var lis = ul.getElementsByTagName("li");
+            for(var i = 0; i < lis.length; i ++){
+                lis[i].style.padding = "10px 20px 10px 20px";
+            }
+            var texts = menu.getElementsByClassName("text");
+            for(var i = 0; i < texts.length; i ++){
+                texts[i].style.display="table-cell";
+            }
             contentWrapper.style.width = width - 300 + "px";
             break;
         default:
