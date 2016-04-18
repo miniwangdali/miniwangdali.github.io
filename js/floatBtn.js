f2b52d07e8ef72f81fe02803a19e9f28486b47ed
miniwangdali <wang__dali@sina.com>
2016-04-19 00:00:07 +0800

    修改分享按钮弹出效果及实现方式，防止一些BUG

--- js/floatBtn.js
+++ js/floatBtn.js
@@ -49,135 +49,112 @@ function onMouseClick(){
 
 function shareOpen(){
     share1 = document.getElementById("share1");
+    share1.style.display = "inline";
     share1.style.animation = "flopen1 0.7s";
     share1.style.webkitAnimation = "flopen1 0.7s";
-    share1.addEventListener("animationend", function(){
+    var share1openfunc = function(){
         share1.style.animation = "";
         share1.style.webkitAnimation = "";
         share1.style.transform = "rotate(360deg)";
-        share1.style.webketTransform = "rotate(360deg)";
+        share1.style.webkitTransform = "rotate(360deg)";
         share1.style.bottom = "140px";
         share1.style.right = "70px";
-        share1.style.visibility = "visible";
-    });
-    share1.addEventListener("webkitAnimationEnd", function(){
-        share1.style.animation = "";
-        share1.style.webkitAnimation = "";
-        share1.style.transform = "rotate(360deg)";
-        share1.style.webketTransform = "rotate(360deg)";
-        share1.style.bottom = "140px";
-        share1.style.right = "70px";
-        share1.style.visibility = "visible";
-    });
+        share1.style.opacity = "1";
+        share1.removeEventListener("animationend", share1openfunc);
+        share1.removeEventListener("webkitAnimationEnd", share1openfunc);
+    };
+    share1.addEventListener("animationend", share1openfunc);
+    share1.addEventListener("webkitAnimationEnd", share1openfunc);
     
     share2 = document.getElementById("share2");
+    share2.style.display = "inline";
     share2.style.animation = "flopen2 0.7s 0.2s";
     share2.style.webkitAnimation = "flopen2 0.7s 0.2s";
-    share2.addEventListener("animationend", function(){
+    var share2openfunc = function(){
         share2.style.animation = "";
         share2.style.webkitAnimation = "";
         share2.style.transform = "rotate(360deg)";
-        share2.style.webketTransform = "rotate(360deg)";
+        share2.style.webkitTransform = "rotate(360deg)";
         share2.style.bottom = "70px";
         share2.style.right = "70px";
-        share2.style.visibility = "visible";
-    });
-    share2.addEventListener("webkitAnimationEnd", function(){
-        share2.style.animation = "";
-        share2.style.webkitAnimation = "";
-        share2.style.transform = "rotate(360deg)";
-        share2.style.webketTransform = "rotate(360deg)";
-        share2.style.bottom = "70px";
-        share2.style.right = "70px";
-        share2.style.visibility = "visible";
-    });
+        share2.style.opacity = "1";
+        share2.removeEventListener("animationend", share2openfunc);
+        share2.removeEventListener("webkitAnimationEnd", share2openfunc);
+    };
+    share2.addEventListener("animationend", share2openfunc);
+    share2.addEventListener("webkitAnimationEnd", share2openfunc);
     
     share3 = document.getElementById("share3");
+    share3.style.display = "inline";
     share3.style.animation = "flopen3 0.7s 0.4s";
     share3.style.webkitAnimation = "flopen3 0.7s 0.4s";
-    share3.addEventListener("animationend", function(){
+    var share3openfunc = function(){
         share3.style.animation = "";
         share3.style.webkitAnimation = "";
         share3.style.transform = "rotate(360deg)";
-        share3.style.webketTransform = "rotate(360deg)";
+        share3.style.webkitTransform = "rotate(360deg)";
         share3.style.bottom = "0px";
         share3.style.right = "70px";
-        share3.style.visibility = "visible";
-    });
-    share1.addEventListener("webkitAnimationEnd", function(){
-        share3.style.animation = "";
-        share3.style.webkitAnimation = "";
-        share3.style.transform = "rotate(360deg)";
-        share3.style.webketTransform = "rotate(360deg)";
-        share3.style.bottom = "0px";
-        share3.style.right = "70px";
-        share3.style.visibility = "visible";
-    });
+        share3.style.opacity = "1";
+        share3.removeEventListener("animationend", share3openfunc);
+        share3.removeEventListener("webkitAnimationEnd", share3openfunc);
+    };
+    share3.addEventListener("animationend", share3openfunc);
+    share3.addEventListener("webkitAnimationEnd", share3openfunc);
 }
 function shareClose(){
     share1 = document.getElementById("share1");
     share1.style.animation = "flclose1 0.7s 0.4s";
     share1.style.webkitAnimation = "flclose1 0.7s 0.4s";
-    share1.addEventListener("animationend", function(){
+    var share1closefunc = function(){
         share1.style.animation = "";
         share1.style.webkitAnimation = "";
         share1.style.transform = "rotate(0deg)";
-        share1.style.webketTransform = "rotate(0deg)";
+        share1.style.webkitTransform = "rotate(0deg)";
         share1.style.bottom = "0px";
         share1.style.right = "0px";
-        share1.style.visibility = "hidden";
-    });
-    share1.addEventListener("webkitAnimationEnd", function(){
-        share1.style.animation = "";
-        share1.style.webkitAnimation = "";
-        share1.style.transform = "rotate(0deg)";
-        share1.style.webketTransform = "rotate(0deg)";
-        share1.style.bottom = "0px";
-        share1.style.right = "0px";
-        share1.style.visibility = "hidden";
-    });
+        share1.style.opacity = "0";
+        share1.style.display = "none";
+        share1.removeEventListener("animationend", share1closefunc);
+        share1.removeEventListener("webkitAnimationEnd", share1closefunc);
+    };
+    share1.addEventListener("animationend", share1closefunc);
+    share1.addEventListener("webkitAnimationEnd", share1closefunc);
     
     share2 = document.getElementById("share2");
     share2.style.animation = "flclose2 0.7s 0.2s";
     share2.style.webkitAnimation = "flclose2 0.7s 0.2s";
-    share2.addEventListener("animationend", function(){
+    var share2closefunc = function(){
         share2.style.animation = "";
         share2.style.webkitAnimation = "";
         share2.style.transform = "rotate(0deg)";
-        share2.style.webketTransform = "rotate(0deg)";
+        share2.style.webkitTransform = "rotate(0deg)";
         share2.style.bottom = "0px";
         share2.style.right = "0px";
-        share2.style.visibility = "hidden";
-    });
-    share2.addEventListener("webkitAnimationEnd", function(){
-        share2.style.animation = "";
-        share2.style.webkitAnimation = "";
-        share2.style.transform = "rotate(0deg)";
-        share2.style.webketTransform = "rotate(0deg)";
-        share2.style.bottom = "0px";
-        share2.style.right = "0px";
-        share2.style.visibility = "hidden";
-    });
+        share2.style.opacity = "0";
+        share2.style.display = "none";
+        share2.removeEventListener("animationend", share2closefunc);
+        share2.removeEventListener("webkitAnimationEnd", share2closefunc);
+    };
+    share2.addEventListener("animationend", share2closefunc);
+    share2.addEventListener("webkitAnimationEnd", share2closefunc);
     
     share3 = document.getElementById("share3");
     share3.style.animation = "flclose3 0.7s";
     share3.style.webkitAnimation = "flclose3 0.7s";
-    share3.addEventListener("animationend", function(){
+    var share3closefunc = function(){
         share3.style.animation = "";
         share3.style.webkitAnimation = "";
         share3.style.transform = "rotate(0deg)";
-        share3.style.webketTransform = "rotate(0deg)";
+        share3.style.webkitTransform = "rotate(0deg)";
         share3.style.bottom = "0px";
         share3.style.right = "0px";
-        share3.style.visibility = "hidden";
-    });
-    share1.addEventListener("webkitAnimationEnd", function(){
-        share3.style.animation = "";
-        share3.style.webkitAnimation = "";
-        share3.style.transform = "rotate(0deg)";
-        share3.style.webketTransform = "rotate(0deg)";
-        share3.style.bottom = "0px";
-        share3.style.right = "0px";
-        share3.style.visibility = "hidden";
-    });
+        share3.style.opacity = "0";
+        share3.style.display = "none";
+        share3.removeEventListener("animationend", share3closefunc);
+        share3.removeEventListener("webkitAnimationEnd", share3closefunc);
+    };
+    share3.addEventListener("animationend", share3closefunc);
+    share3.addEventListener("webkitAnimationEnd", share3closefunc);
+    
 }
