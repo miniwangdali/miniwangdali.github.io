9ce148a3209280935e14ae28e6259f0ec365dabc
miniwangdali <wang__dali@sina.com>
2016-04-26 08:18:02 +0800

    修复日期变更问题

--- js/calendar.js
+++ js/calendar.js
@@ -26,16 +26,36 @@ function calendarInit(){
     year = date.getFullYear();
     month = date.getMonth();
     today = date.getDate();
+    generateDates(date);
+    
+    previousYear.innerHTML = year - 1;
+    currentYear.innerHTML = year;
+    nextYear.innerHTML = year + 1;
+    
+    currentMonth.innerHTML = months[month];
+}
+
+function generateDates(date){
     var firstday = new Date(date.setDate(1));
     var first = firstday.getDay();
     var num = 1;
     var i = first;
+    var monthChanged = false;
     while(i < daylist.length){
         firstday = new Date(firstday.setDate(num));
         daylist[i].innerHTML = firstday.getDate();
-        if(daylist[i].className != "weekends"){
-            daylist[i].className = "this-month";
+        if(daylist[i].className == "today"){
+                if(firstday.getDay() == 0 || firstday.getDay() == 6){
+                    daylist[i].className = "weekends";
+                }else{
+                    daylist[i].className = "this-month";
+                }
+        }else if(daylist[i].className != "weekends"){
+            if(monthChanged) daylist[i].className = "other-month";
+            else daylist[i].className = "this-month";
         }
+        if(num == today) daylist[i].className = "today";
+        if(num - firstday.getDate() > 0) monthChanged = true;
         num = firstday.getDate() + 1;
         i ++;
     }
@@ -53,9 +73,41 @@ function calendarInit(){
             i --;
         }
     }
+}
+
+function changeDate(newYear,newMonth){
+    var date = new Date();
+    date = new Date(date.setFullYear(newYear, newMonth));
+    year = date.getFullYear();
+    month = date.getMonth();
+    today = date.getDate();
+    generateDates(date);
+}
+function premonth(){
+    changeDate(year, month - 1);
+    currentMonth.innerHTML = months[month];
     previousYear.innerHTML = year - 1;
     currentYear.innerHTML = year;
     nextYear.innerHTML = year + 1;
-    
+}
+function nextmonth(){
+    changeDate(year, month + 1);
     currentMonth.innerHTML = months[month];
+    previousYear.innerHTML = year - 1;
+    currentYear.innerHTML = year;
+    nextYear.innerHTML = year + 1;
+}
+function preyear(){
+    changeDate(year - 1, month);
+    currentMonth.innerHTML = months[month];
+    previousYear.innerHTML = year - 1;
+    currentYear.innerHTML = year;
+    nextYear.innerHTML = year + 1;
+}
+function nextyear(){
+    changeDate(year + 1, month);
+    currentMonth.innerHTML = months[month];
+    previousYear.innerHTML = year - 1;
+    currentYear.innerHTML = year;
+    nextYear.innerHTML = year + 1;
 }
